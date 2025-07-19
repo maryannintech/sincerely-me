a65daf7071e6861b3df760cdd6204e9331c4d365
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-19 18:24:52 +0800

    feat: implement SignIn component and update App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { SignIn } from './pages/SignIn'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+     <SignIn />
     </>
   )
 }

5647e97362989d3ebd87f0c73fc00175eec3aaca
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-21 09:55:43 +0800

    style: update Loading component to italicize loading message for improved emphasis

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -8,7 +8,7 @@ export function Loading() {
         <img src="src/assets/images/logo.png" className="w-20 sm:w-40"></img>
         <p className="ml-3 sm:ml-5 text-2xl sm:text-5xl">Sincerely, Me</p>
       </div>
-      <p className="mt-10 sm:mt-20 font-bold animate-pulse sm:text-xl text-right">Preparing your letters...</p>
+      <p className="mt-10 sm:mt-20 italic font-bold animate-pulse sm:text-xl text-right">Preparing your letters...</p>
       <Footer />
     </div>
   );

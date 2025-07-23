263fbf690a569f0b31fd661422e82b27e237f18c
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-23 16:58:38 +0800

    feat: enhance Navbar component with new icon styling and hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,21 @@ export function Navbar() {
       <div>
         <img src="/images/logo.png" className="hidden sm:block w-15 "></img>
       </div>
-      <div className="text-[var(--primary-color)] sm:text-4xl text-3xl flex sm:flex-col justify-center gap-7 sm:gap-3 fixed bottom-0 sm:top-0 py-3 bg-[var(--cream-color)] sm:bg-transparent border-t-2 sm:border-0 border-[var(--primary-color)] w-full sm:w-auto">
+      <div className="text-[var(--primary-color)] sm:text-4xl text-3xl flex sm:flex-col justify-center gap-4 sm:gap-3 fixed bottom-0 sm:top-0 pt-3 pb-1 bg-[var(--cream-color)] sm:bg-transparent border-t-2 sm:border-0 border-[var(--primary-color)] w-full sm:w-auto">
         <Link to={"/app/dashboard"}>
-          <i className="bx  bxs-home-heart cursor-pointer"></i>
+          <i className="bx bxs-home-heart cursor-pointer navbar-icon"></i>
         </Link>
         <Link to={"/app/write"}>
-          <i className="bx  bxs-plus-square cursor-pointer"></i>
+          <i className="bx bxs-plus-square cursor-pointer navbar-icon"></i>
         </Link>
         <Link to={"/app/all-letters"}>
-          <i className="bx  bxs-grid-search cursor-pointer"></i>
+          <i className="bx bxs-grid-search cursor-pointer navbar-icon"></i>
         </Link>
         <Link to={"/app/about"}>
-          <i className="bx  bxs-info-circle cursor-pointer"></i>
+          <i className="bx bxs-info-circle cursor-pointer navbar-icon"></i>
         </Link>
         <Link to={"/signin"}>
-        <i className='bx  bx-arrow-out-up-square-half rotate-90'  ></i> 
+          <i className="bx bx-arrow-out-up-square-half rotate-90 cursor-pointer navbar-icon"></i>
         </Link>
       </div>
     </div>

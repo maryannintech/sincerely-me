7612657b13a489a23f4fe56928ebff5549870db7
mary ann <114210058+maryannintech@users.noreply.github.com>
2025-07-31 19:45:36 +0800

    fix: update loading message and adjust class structure for improved styling

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -3,13 +3,13 @@ import { Footer } from "../components/Footer";
 export function Loading() {
   document.title = "Sincerely, Me";
   return (
-    <div className="flex flex-col justify-center items-center m-auto h-screen select-none soft-popup">
-      <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center m-auto h-screen select-none ">
+      <div className="flex justify-center items-center soft-popup">
         <img src="/images/logo.png" className="w-20 sm:w-40"></img>
         <p className="ml-3 sm:ml-5 text-2xl sm:text-5xl">Sincerely, Me</p>
       </div>
       <p className="mt-10 sm:mt-20 italic font-bold animate-pulse sm:text-xl text-right text-[var(--primary-color)]">
-        Preparing your letters...
+        Opening your mailbox...
       </p>
       <Footer />
     </div>
